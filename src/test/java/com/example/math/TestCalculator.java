f90829b8de7ec0d48129a52a991e211a69d7d303
Jay McGavren <jay@mcgavren.com>
2019-02-09 19:25:08 -0700

    Add Calculator class

---
+++ src/test/java/com/example/math/TestCalculator.java
@@ -0,0 +1,20 @@
+package com.example.math;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void add() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void subtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(8, 6));
+    }
+
+}
