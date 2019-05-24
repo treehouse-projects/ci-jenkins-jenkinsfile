d2ec492199c24aeada96ae14bb0a17363b4c45e2
Omari <20199517+omarirose@users.noreply.github.com>
2019-05-24 00:08:40 -0400

    Added third test
    add test to testcalculator class

--- src/test/java/com/example/math/TestCalculator.java
+++ src/test/java/com/example/math/TestCalculator.java
@@ -16,5 +16,11 @@ public class TestCalculator {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.subtract(8, 6));
     }
+    
+    @Test
+    public void multiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(20, calculator.multiply(10, 2));
+    }
 
 }
